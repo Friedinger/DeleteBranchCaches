f6a765f2442f5bacf11a2b6b376b0e268d3fd2cf
Friedinger <71267611+Friedinger@users.noreply.github.com>
2025-10-29 20:28:46 +0100

    🧑‍💻 Define test coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,16 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
     test: {
         globals: true,
+        coverage: {
+            provider: "v8",
+            exclude: ["test/**"],
+            thresholds: {
+                branches: 100,
+                functions: 100,
+                lines: 100,
+                statements: 100,
+                perFile: true,
+            },
+        },
     },
 });
