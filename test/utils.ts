c895fa2c99a7025ea27fd5598b2e500f7794409b
Friedinger <71267611+Friedinger@users.noreply.github.com>
2026-02-22 14:31:14 +0100

    🎨 Format code with prettier

--- test/utils.ts
+++ test/utils.ts
@@ -3,56 +3,56 @@ import { vi } from "vitest";
 export type CacheEntry = { id: number; size_in_bytes?: number };
 
 export const runAction = async () => {
-    await import("../src/index");
+  await import("../src/index");
 };
 
 export const setupOctokitMocks = (
-    octokit: any,
-    responses: Array<CacheEntry> | Array<Array<CacheEntry>>
+  octokit: any,
+  responses: Array<CacheEntry> | Array<Array<CacheEntry>>,
 ) => {
-    const { getActionsCacheList, deleteActionsCacheById } = makeActionsMocks(
-        responses as any
-    );
-    vi.mocked(octokit).mockImplementation(function (): any {
-        return {
-            rest: {
-                actions: {
-                    getActionsCacheList,
-                    deleteActionsCacheById,
-                },
-            },
-        };
-    });
-    return { getActionsCacheList, deleteActionsCacheById };
+  const { getActionsCacheList, deleteActionsCacheById } = makeActionsMocks(
+    responses as any,
+  );
+  vi.mocked(octokit).mockImplementation(function (): any {
+    return {
+      rest: {
+        actions: {
+          getActionsCacheList,
+          deleteActionsCacheById,
+        },
+      },
+    };
+  });
+  return { getActionsCacheList, deleteActionsCacheById };
 };
 
 export const makeActionsMocks = (
-    responses: Array<CacheEntry> | Array<Array<CacheEntry>>
+  responses: Array<CacheEntry> | Array<Array<CacheEntry>>,
 ) => {
-    let getActionsCacheList: any;
+  let getActionsCacheList: any;
 
-    if (
-        Array.isArray(responses) &&
-        responses.length &&
-        Array.isArray(responses[0])
-    ) {
-        getActionsCacheList = vi.fn();
-        for (const caches of responses as Array<Array<any>>) {
-            getActionsCacheList.mockResolvedValueOnce({
-                data: {
-                    total_count: caches.length,
-                    actions_caches: caches,
-                },
-            });
-        }
-    } else {
-        const caches = responses as Array<any>;
-        getActionsCacheList = vi.fn().mockResolvedValue({
-            data: { total_count: caches.length, actions_caches: caches },
-        });
+  if (
+    Array.isArray(responses) &&
+    responses.length &&
+    Array.isArray(responses[0])
+  ) {
+    getActionsCacheList = vi.fn();
+    for (const caches of responses as Array<Array<any>>) {
+      getActionsCacheList.mockResolvedValueOnce({
+        data: {
+          total_count: caches.length,
+          actions_caches: caches,
+        },
+      });
     }
+  } else {
+    const caches = responses as Array<any>;
+    getActionsCacheList = vi.fn().mockResolvedValue({
+      data: { total_count: caches.length, actions_caches: caches },
+    });
+  }
 
-    const deleteActionsCacheById = vi.fn().mockResolvedValue({});
+  const deleteActionsCacheById = vi.fn().mockResolvedValue({});
 
-    return { getActionsCacheList, deleteActionsCacheById };
+  return { getActionsCacheList, deleteActionsCacheById };
 };
